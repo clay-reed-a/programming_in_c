4ce36505d6e28f7ee12243a3ace11ec7fe484bfa
clay-reed-a <clay.reed.a@gmail.com>
2015-02-19 00:42:13 -0500

    Variables!

---
+++ chapter_03/ex1/prog3-5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (void)
+{
+  int three = 2,
+        two = 2;
+
+  int logic = three + two; 
+
+  printf("%i and %i are %i\n", three, two, logic+1);
+
+  return 0; 
+}
